3ae73108017af26159e23b966192d109a402b360
Rachel <106885713+rachview@users.noreply.github.com>
2022-08-24 15:30:11 -0400

    Javascript: Swiper
    Javascript: Swiper carousel with Adrian Twarog (Youtube)

---
+++ swiperCarousel/Cube/js/index.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
---
+++ swiperCarousel/Parallax/js/index.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  speed: 600,
+  parallax: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
